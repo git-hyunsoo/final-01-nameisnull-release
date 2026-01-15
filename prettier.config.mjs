739fd3c2145eac3710d143e35fe793cd2c28a53b
git-hyunsoo <soo1991@gmail.com>
2026-01-15 14:11:44 +0000

    프리티어 추가

---
+++ prettier.config.mjs
@@ -0,0 +1,14 @@
+const prettierConfig = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 80,
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
+
+export default prettierConfig;
